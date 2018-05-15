d6411bb614562b73516bdf51440b7770d7a99f34
Achilleas Koutsou <achilleas.k@gmail.com>
2018-05-15 14:03:16 +0200

    mkfile: Script to create sample file for NIX Raw IO

---
+++ mkfile.py
@@ -0,0 +1,87 @@
+from neo import (Block, Segment,
+                 AnalogSignal, IrregularlySampledSignal,
+                 Event, Epoch, SpikeTrain,
+                 ChannelIndex, Unit)
+from neo.io.nixio import NixIO
+
+import numpy as np
+import quantities as pq
+
+
+nsegments = 1
+
+block = Block("nix-raw-block", description="The root block")
+
+for idx in range(nsegments):
+    seg = Segment("seg-ex{}".format(idx),
+                  description="Segment number {}".format(idx))
+    block.segments.append(seg)
+
+    # Generate 3 fake data signals using numpy's random function
+    # The shapes of the arrays are arbitrary
+    data_a = np.random.random((300, 2))
+    data_b = np.random.random((1200, 3))
+    data_c = np.random.random((8000, 5))
+    nchannels = data_a.shape[1] + data_b.shape[1] + data_c.shape[1]
+
+    sampling_rate = pq.Quantity(10, "Hz")
+
+    for didx, data in enumerate((data_a, data_b, data_c)):
+        asig = AnalogSignal(name="Seg {} :: Data {}".format(idx, didx),
+                            signal=data, units="mV",
+                            sampling_rate=sampling_rate)
+        seg.analogsignals.append(asig)
+
+    # random sampling times for data_b
+    irsigdata = np.random.random((1200, 2))
+    itimes = np.cumsum(np.random.random(1200))
+
+    # Create one AnalogSignal and one IrregularlySampledSignal
+    isig = IrregularlySampledSignal(name="Sampled data", signal=irsigdata,
+                                    units="nA",
+                                    times=itimes, time_units="ms")
+    seg.irregularlysampledsignals.append(isig)
+
+    # Event, Epoch, SpikeTrain
+    tstart = 10 * pq.ms
+    event_times = tstart + np.cumsum(np.random.random(5)) * pq.ms
+    event = Event(name="Seg {} :: Event".format(idx),
+                  times=event_times,
+                  labels=["A", "B", "C", "D", "E"])
+    seg.events.append(event)
+
+    epoch_times = tstart + np.cumsum(np.random.random(3)) * pq.ms
+    epoch = Epoch(name="Seg {} :: Epoch".format(idx),
+                  times=epoch_times,
+                  durations=np.random.random(3)*pq.ms,
+                  labels=["A+", "B+", "C+"])
+    seg.epochs.append(epoch)
+
+    st_times = tstart + np.cumsum(np.random.random(10)) * pq.ms
+    tstop = max(event_times[-1], epoch_times[-1], st_times[-1]) + 1 * pq.ms
+    st = SpikeTrain(name="Seg {} :: SpikeTrain".format(idx),
+                    times=st_times, t_start=tstart, t_stop=tstop)
+    wf = np.random.random((len(st_times), nchannels, 30)) * pq.mV
+    st.waveforms = wf
+    st.sampling_rate = sampling_rate
+
+    seg.spiketrains.append(st)
+
+    indexes = np.arange(nchannels)
+    chx = ChannelIndex(name="channel-{}".format(idx),
+                       index=indexes,
+                       channel_names=[chr(ord("a") + i) for i in indexes],
+                       channel_ids=indexes+1)
+    block.channel_indexes.append(chx)
+    chx.analogsignals.extend(seg.analogsignals)
+
+    unit = Unit(name="unit-{}".format(idx))
+    unit.spiketrains.append(st)
+    chx.units.append(unit)
+
+# Write the Block to file using the NixIO
+# Any existing file will be overwritten
+io = NixIO("neoraw.nix", "ow")
+io.write_block(block)
+
+io.close()
